3a818849fad8445dd5de0d9d1121eceae9da8264
Moisas <mopri08@gmail.com>
2021-10-20 13:53:41 -0300

    criação dos services das DI's

---
+++ src/services/di.service.js
@@ -0,0 +1,52 @@
+import DiRepository from '../repositories/di.repository.js'
+
+async function criarDi(dadosDi){
+    try {
+        // passando as letras para maiusculas
+        dadosDi.os = dadosDi.os.toUpperCase()
+        dadosDi.numero_pedido = dadosDi.numero_pedido.toUpperCase()
+        dadosDi.numero_orcamento = dadosDi.numero_orcamento.toUpperCase()
+        dadosDi.descricao = dadosDi.descricao.toUpperCase()
+        dadosDi.numero = dadosDi.numero.toUpperCase()
+        dadosDi.op = dadosDi.op.toUpperCase()
+        dadosDi.nome_peca = dadosDi.nome_peca.toUpperCase()
+        dadosDi.numero_peca = dadosDi.numero_peca.toUpperCase()
+
+        return await DiRepository.criarDi(dadosDi)
+    } catch (error) {
+        throw error
+    }
+}
+
+async function alterarDi(dadosDi){
+    try {
+        // passando as letras para maiusculas
+        dadosDi.os = dadosDi.os.toUpperCase()
+        dadosDi.numero_pedido = dadosDi.numero_pedido.toUpperCase()
+        dadosDi.numero_orcamento = dadosDi.numero_orcamento.toUpperCase()
+        dadosDi.descricao = dadosDi.descricao.toUpperCase()
+        dadosDi.numero = dadosDi.numero.toUpperCase()
+        dadosDi.op = dadosDi.op.toUpperCase()
+        dadosDi.nome_peca = dadosDi.nome_peca.toUpperCase()
+        dadosDi.numero_peca = dadosDi.numero_peca.toUpperCase()
+
+        return await DiRepository.alterarDi(dadosDi)
+    } catch (error) {
+        throw error
+    }
+}
+
+async function buscaDi(di){
+    return await DiRepository.buscaDi(di)
+}
+
+async function buscaTodasDi(){
+    return await DiRepository.buscaTodasDi()
+}
+
+export default {
+    criarDi,
+    alterarDi,
+    buscaDi,
+    buscaTodasDi
+}
